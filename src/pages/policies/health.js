42fa728d44801c15164b7551d22bef731b1ceaea
Chris Barnett <chris.d.barnett@googlemail.com>
2020-01-05 00:40:23 +0000

    initial commit

---
+++ src/pages/policies/health.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const PolicyHealthPage = () => (
+
+    <Layout>
+
+        <div>
+
+            <h2>The NHS is a great British social institution that we all should be proud of. It belongs to the British people who pay for it and not to health tourists taking advantage of Soft Touch Britain.</h2>
+            <p><strong>We will:</strong></p>
+            <ul>
+            <li><strong>Fight for a public funded and provided health service free at the point of use</strong></li>
+            <li><strong>Fill the current &pound;8billion gap in the health service with part of the 12.5billion we save on foreign aid</strong></li>
+            <li><strong>Stop &lsquo;health tourism&rsquo;</strong></li>
+            <li><strong>Visitors to Britain will be required to have a valid, Government recognised health insurance policy before entry, just as British citizens must have when visiting other countries</strong></li>
+            <li><strong>Bring hospital cleaning back in-house and make high cleanliness a top priority</strong></li>
+            <li><strong>Scrap hospital parking charges</strong></li>
+            <li><strong>Halt privatisation, the NHS will remain a cherished public-owned institution under a BNP Government</strong></li>
+            <li><strong>Cut bureaucracy, not nurses&rsquo; jobs and hospitals</strong></li>
+            <li><strong>Free GPs to see patients, not manage budget sheets</strong></li>
+            <li><strong>Emphasis on prevention and early diagnosis</strong></li>
+            </ul>
+            <p>For decades, the British National Health Service was looked upon by the rest of the world as one of the most successful state run health services in the world.</p>
+            <p>Today, it is a laughing stock. The NHS is critically ill. NHS Trusts are sacking staff, closing wards, cancelling operations and refusing patients vital life-saving drugs. Meanwhile, &lsquo;health tourists&rsquo; are costing the NHS &pound;2 billion a year, and diseases such as TB and AIDS are on the increase as a result of immigration.</p>
+            <p>British doctors and nurses are unable to find work in the NHS because we are importing medical staff (often with questionable qualifications) from the Third World where they are desperately needed.</p>
+            <p>Sixty percent of NHS staff are bureaucrats, and there are now more managers in the NHS than beds &mdash; with many earning more than &pound;100,000 per year.</p>
+            <p>The contracting out of cleaning (often to contractors employing illegal immigrants) has resulted in collapsing standards of cleanliness, with around 20,000 patients a year dying of hospital-acquired infections.</p>
+            <p>&ldquo;Marketisation,&rdquo; and particularly the Conservative-created Private Finance Initiative (PFI) schemes imposed by Gordon Brown, has been a disaster which is saddling Trusts and the taxpayer with enormous debts. We totally reject this attempt to turn the nation&rsquo;s health service into a private profit centre for giant corporations.</p>
+            <p>The British National Party has the only plan by which our NHS can be nursed back to health.</p>
+            <p>The BNP is wholly committed to a free, fully funded National Health Service for all British citizens.</p>
+        
+        </div>
+
+    </Layout>
+
+)
+
+export default PolicyHealthPage
+
---
+++ src/pages/policies/lawandorder.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const PolicyLawAndOrderPage = () => (
+    <Layout>
+      <div>
+        <h1>Policy - Law and Order</h1>
+        <h2>One of the key tests of a state is its ability to protect its citizens. It is vital that our people feel safe and secure.</h2>
+        <h2>No one should have to fear that their property or person will be unlawfully interfered with. We must restore public confidence in our justice system.</h2>
+        <p>Britain suffers from more than 27,000 crimes per day, or more than ten million crimes per year &ndash; the direct result of decades of softly-softly politically correct policing and a failed social engineering policy which has put the rights of yobs and criminals above that of victims.</p>
+        <p>According to the Office for National Statistics&rsquo; British Crime Survey of 2008, 22 percent of people in Britain will be the victim of some crime during the course of a single year.</p>
+        <p>To combat this shocking state of affairs, the British National Party seeks a return to traditional standards of law enforcement, combined with social reform directed at addressing the root causes of criminal behaviour.</p>
+        <p><strong>To this end, BNP crime and justice policy will:</strong></p>
+        <ul>
+          <li><strong>Restore Capital Punishment as an option for judges in extreme cases, such as child killers and terrorists</strong></li>
+          <li><strong>Crack-down on crime, with more police on the streets</strong></li>
+          <li><strong>Implement zero tolerance for drug-dealers and violent thugs</strong></li>
+          <li><strong>Tougher treatment of criminals</strong></li>
+          <li><strong>Ensure a separation between mental health and criminal problems</strong></li>
+          <li><strong>Introduce compulsory treatment for drug addicts who have committed crimes</strong></li>
+          <li><strong>Invest in projects to design out crime by better planning of neighbourhoods and products</strong></li>
+        </ul>
+        <p>At the same time, the BNP recognises that decades of social welfare dependence &ndash; encouraged by disastrous Labour and Tory policies &ndash; which is the primary cause of social delinquency, must be brought to an end.</p>
+        <p>Social reform is therefore also required. Workfare, not welfare, except to the neediest, should be the norm. Only in this way can the cycle of social deprivation, which is the primary cause of indigenous criminality, be broken.</p>
+        <p>To this end, the BNP will introduce a system of workfare for those in unemployment benefit for more than six months with compulsory work and training in return for decent payment.</p>
+        <p>The socialist building block housing concrete monstrosities which blight our urban areas and which are the breeding ground for delinquency and crime will be torn down and replaced with decent housing which encourages the stable family unit.</p>
+        <p>Non-indigenous crime &ndash; rapidly becoming a serious problem, as the knife and gun crime epidemic spreading throughout our major cities &ndash; will be dealt with in terms of BNP immigration and identity policy.</p>
+        <p>Overcrowding in prisons will be solved by the deportation of the tens of thousands of foreign criminals to serve their sentences abroad in their home countries. This act alone will free up to 70 percent of jail space in many prisons.</p>
+        <p><strong>The BNP will also:</strong></p>
+        <ul>
+        <li><strong>Make prisons more austere and make criminals serve their full sentences. Offenders will be made to understand that they are being punished and not rewarded with a state-subsidised holiday for their crimes</strong></li>
+        <li><strong>Use electronically tagged &ldquo;chain gangs&rdquo; to provide labour for projects such as coastal defences;</strong></li>
+        <li><strong>Introduce automatic prison sentences for all repeat offenders;</strong></li>
+        <li><strong>Put police back on the streets and remove their current political correctness shackles;</strong></li>
+        <li><strong>Allow victims of crime full freedom to defend themselves and their property;</strong></li>
+        <li><strong>Make joint custody of children the norm in divorce cases;</strong></li>
+        <li><strong>Grant anonymity to those accused of crimes until they are convicted;</strong></li>
+        <li><strong>Make police concentrate on real criminals and serve the public, not the government&rsquo;s political aspirations.</strong></li>
+        </ul>
+        <p>The British National Party alone recognises that crime must be tackled on two levels: firstly by effective policing, and secondly by addressing the root social causes of crime, both indigenous and non-indigenous in origin.</p>
+        <p>Addressing these two issues will solve the crime problem.&nbsp;The time has come for change.</p>
+      </div>
+    </Layout>
+  )
+  
+  export default PolicyLawAndOrderPage
+
