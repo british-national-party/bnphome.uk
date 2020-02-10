7752bf9caf5999ccdb3e2dadf5a65fb0bb45c42c
Chris Barnett <chris.d.barnett@googlemail.com>
2020-02-10 22:12:05 +0000

    favicon config2

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,38 @@ module.exports = {
     },    
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-favicon',
     {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: "./src/favicon.png",
+   
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+   
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+       {
       // Removes unused css rules
       resolve:'gatsby-plugin-purgecss',
       options: {
@@ -41,6 +71,7 @@ module.exports = {
         purgeOnly: ['/all.sass'],
       },
     }, // must be after other CSS plugins
+    
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
