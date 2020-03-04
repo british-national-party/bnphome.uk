77682479a56e27a9df0386b9c7785586e665ad26
Chris Barnett <chris.d.barnett@googlemail.com>
2020-03-04 05:32:56 +0000

    Constitution link to footer + Page width fix

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+export default () => (
+    <footer>
+    <div className="footer">
+        <div className="footer-container">
+      
+            <div className="copyright">(C) 2020 British National Party</div>
+            <div className="address">PO Box 9128, Lutterworth, Leicestershire, LE17 9DD, United Kingdom.</div>
+            <div className="constitution"><a href="https://cdn1.bnphome.uk/media-files/2020/03/04051507/constitutionbnp14-2.pdf" target="_blank">Constitution</a></div>
+            
+        </div>
+    </div>
+    </footer>
+  )
